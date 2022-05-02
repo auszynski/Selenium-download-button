fd66f1e0df7b95fb87d8d22c7090fabd8fac73eb
Lysech <arturuszynski13@gmail.com>
2022-05-02 18:22:31 +0200

    test

--- from selenium import webdriver.py
+++ from selenium import webdriver.py
@@ -1,6 +1,9 @@
-import os
+#import os
+#from selenium import webdriver
+
+#os.environ['PATH'] += r"/Users/Artur/Downloads/chromedriver"
+#driver = webdriver.Chrome()
+
 from selenium import webdriver
-
-os.environ['PATH'] += r"/Users/Artur/Downloads/chromedriver"
-driver = webdriver.Chrome()
-
+driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
+driver.get("https://www.google.pl/index.html")
